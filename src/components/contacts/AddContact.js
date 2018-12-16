bd3bc42e7e21693cbabb55e1c22b42934006624f
Albert Sanchez <al@Alberts-MacBook-Pro.local>
2018-12-16 15:40:25 -0600

    added contact add fields with bootstrap

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
 
 class AddContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: ''
+  }
+
   render() {
     return (
-      <div>
-        <h1>Add Contact</h1>
+      <div className="card mb-3">
+        <div className="card-header">
+          Add Contact</div>
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control form-control-lg"
+                  placeholder="Enter Name..."
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control form-control-lg"
+                  placeholder="Enter Email..."
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="name">Phone</label>
+                <input
+                  type="text"
+                  name="phone"
+                  className="form-control form-control-lg"
+                  placeholder="Enter Phone..."
+                />
+              </div>
+            </form>
+          </div>
       </div>
     )
   }
