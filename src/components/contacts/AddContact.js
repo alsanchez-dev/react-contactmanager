6deebd72f915c1f736af91c6aca034fc219b3e9a
Albert Sanchez <al@Alberts-MacBook-Pro.local>
2018-12-16 22:08:34 -0600

    Added submit button that calls the submit method, fields are working well

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,13 +7,21 @@ class AddContact extends Component {
     phone: ''
   }
 
+onSubmit = (e) => {
+  e.preventDefault();
+  console.log(this.state);
+}
+
+onChange = e => this.setState({ [e.target.name]:
+  e.target.value });
+
   render() {
+    const {name, email, phone} = this.state;
     return (
       <div className="card mb-3">
-        <div className="card-header">
-          Add Contact</div>
+        <div className="card-header">Add Contact</div>
           <div className="card-body">
-            <form>
+            <form onSubmit={this.onSubmit}>
               <div className="form-group">
                 <label htmlFor="name">Name</label>
                 <input
@@ -21,6 +29,8 @@ class AddContact extends Component {
                   name="name"
                   className="form-control form-control-lg"
                   placeholder="Enter Name..."
+                  value={name}
+                  onChange={this.onChange}
                 />
               </div>
               <div className="form-group">
@@ -30,6 +40,8 @@ class AddContact extends Component {
                   name="email"
                   className="form-control form-control-lg"
                   placeholder="Enter Email..."
+                  value={email}
+                  onChange={this.onChange}
                 />
               </div>
               <div className="form-group">
@@ -39,6 +51,8 @@ class AddContact extends Component {
                   name="phone"
                   className="form-control form-control-lg"
                   placeholder="Enter Phone..."
+                  value={phone}
+                  onChange={this.onChange}
                 />
               </div>
               <input type="submit" value="Add Contact"
