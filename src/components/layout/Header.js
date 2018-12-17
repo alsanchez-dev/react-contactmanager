6abf3d9b64fb2f44687ed8c535e18f8855d43357
Albert Sanchez <al@Alberts-MacBook-Pro.local>
2018-12-17 13:06:04 -0600

    added router and nav nav bar with home

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Header = (props)=>{
   const { branding } = props;
@@ -11,7 +12,7 @@ const Header = (props)=>{
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a href="/" className="nav-link">Home</a>
+              <Link to="/" className="nav-link">Home</Link>
             </li>
           </ul>
         </div>
