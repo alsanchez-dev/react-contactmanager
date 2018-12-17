28f661779e8509ba177c5395f995acb8c7613e69
Albert Sanchez <al@Alberts-MacBook-Pro.local>
2018-12-17 10:38:07 -0600

    added validation for each text field

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,13 +7,44 @@ class AddContact extends Component {
   state = {
     name: '',
     email: '',
-    phone: ''
+    phone: '',
+    errors: {}
   }
 
 onSubmit = (dispatch, e) => {
   e.preventDefault()
 
-  const {name, email, phone} = this.state
+  const {name, email, phone} = this.state;
+
+
+  // Check for errors
+  if(name === ''){
+    this.setState(
+      {
+        errors:{ name: 'Name is required' }
+      }
+    );
+    return;
+  }
+
+  if(email === ''){
+    this.setState(
+      {
+        errors:{ email: 'Email is required' }
+      }
+    );
+    return;
+  }
+
+  if(phone === ''){
+    this.setState(
+      {
+        errors:{ phone: 'Phone is required' }
+      }
+    );
+    return;
+  }
+
 
   const newContact = {
     id: uuid(),
@@ -28,8 +59,9 @@ onSubmit = (dispatch, e) => {
   this.setState({
     name: '',
     email: '',
-    phone: ''
-  })
+    phone: '',
+    errors: {}
+  });
 
 }
 
@@ -37,7 +69,7 @@ onChange = e => this.setState({ [e.target.name]:
   e.target.value });
 
   render() {
-    const {name, email, phone} = this.state;
+    const {name, email, phone, errors} = this.state;
 
     return (
       <Consumer>
@@ -54,6 +86,7 @@ onChange = e => this.setState({ [e.target.name]:
                   placeholder="Enter Name"
                   value={name}
                   onChange={this.onChange}
+                  error={errors.name}
                 />
                   <TextInputGroup
                   label="Email"
@@ -62,6 +95,7 @@ onChange = e => this.setState({ [e.target.name]:
                   placeholder="Enter Email"
                   value={email}
                   onChange={this.onChange}
+                  error={errors.email}
                 />
                   <TextInputGroup
                   label="Phone"
@@ -69,6 +103,7 @@ onChange = e => this.setState({ [e.target.name]:
                   placeholder="Enter Phone"
                   value={phone}
                   onChange={this.onChange}
+                  error={errors.phone}
                 />
                 <input type="submit" value="Add Contact"
                   className="btn btn-light btn-block"/>
