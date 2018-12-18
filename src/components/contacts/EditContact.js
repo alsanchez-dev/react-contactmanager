b67aaf0ea61c292bc250983d7ad3f039d77261b4
Albert Sanchez <al@Alberts-MacBook-Pro.local>
2018-12-18 16:53:25 -0600

    the app is now updating and submiting updates

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -59,6 +59,20 @@ onSubmit = async (dispatch, e) => {
     return;
   }
 
+  const updContact ={
+    name,
+    email,
+    phone
+  }
+
+  const { id } = this.props.match.params;
+
+  const res = await axios
+    .put(`https://jsonplaceholder.typicode.com/users/${id}`, updContact);
+
+    dispatch({type: 'UPDATE_CONTACT', payload: res.data});
+
+
   // Clear State
   this.setState({
     name: '',
