a6301b34cd5021013acad016ce1838563dbbb804
Albert Sanchez <al@Alberts-MacBook-Pro.local>
2018-12-18 18:39:08 -0600

    Updates

---
+++ precache-manifest.2dc7ba048f15aca1991755afbc5523f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e40d23b8b61876c686f",
+    "url": "/react-contactmanager/static/js/runtime~main.3e40d23b.js"
+  },
+  {
+    "revision": "c09e4d859fe67430a806",
+    "url": "/react-contactmanager/static/js/main.c09e4d85.chunk.js"
+  },
+  {
+    "revision": "d7eee9dc8d3699cf77c3",
+    "url": "/react-contactmanager/static/js/1.d7eee9dc.chunk.js"
+  },
+  {
+    "revision": "c09e4d859fe67430a806",
+    "url": "/react-contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "d7eee9dc8d3699cf77c3",
+    "url": "/react-contactmanager/static/css/1.03d3328f.chunk.css"
+  },
+  {
+    "revision": "e165b007cbfbd43d42cf34519ba6cdf2",
+    "url": "/react-contactmanager/index.html"
+  }
+];
